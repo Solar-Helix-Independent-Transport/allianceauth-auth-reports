16e8be35717028a0fceb941534d7357c80de4af2
Joel Falknau <joel.falknau@gmail.com>
2024-08-31 18:13:04 +1000

    add basic translation support

---
+++ frontend/i18next-parser.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  locales: ["en"],
+  output: "locales/$LOCALE.$NAMESPACE.json",
+  namespaceSeparator: "::",
+  resetDefaultValueLocale: "en",
+  defaultValue: (locale, namespace, key) => key,
+  input: [
+    "src/**/*.{js,jsx,ts,tsx}",
+  ],
+};
