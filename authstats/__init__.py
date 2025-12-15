9574c38127e762f119af918353d4a0a281b40e79
Aaron Kable <aaronkable@gmail.com>
2025-12-15 19:27:08 +0800

    vbump

--- authstats/__init__.py
+++ authstats/__init__.py
@@ -1,6 +1,6 @@
 """
     Configurable Report Framework for corporate level aggregation of any data
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __title__ = "AuthStats"
 __url__ = "https://github.com/Solar-Helix-Independent-Transport/allianceauth-auth-reports"
